f5737eccab03a3f1fa7eb70a1aa3d7625006f7bf
Gagan <g.deepsingh1@gmail.com>
2019-04-24 02:03:36 +0530

    xcode bulls eye project start

---
+++ BullyEye/BullyEye/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BullyEye
+//
+//  Created by Gagandeep Singh on 24/04/19.
+//  Copyright © 2019 Gagandeep Singh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
